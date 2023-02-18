59ef966655105233c3d5a381d7c7fb6bf95bd007
Satish Kumar Sharma <51714932+skwebs@users.noreply.github.com>
2023-02-18 05:35:54 +0530

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,12 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
